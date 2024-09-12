ed81f113cd430aeda5f90c5ddd588cee3712ec69
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-12 22:09:39 +0530

    JWT Authentication

--- services/auth.js
+++ services/auth.js
@@ -1,11 +1,29 @@
-const sessionIdToUserMap = new Map();
+// const sessionIdToUserMap = new Map();
+const jwt = require('jsonwebtoken');
 
-function setUser(id, user){
-    sessionIdToUserMap.set(id, user);
+const secret = "S@iyam123"
+
+function setUser(user){
+    // function setUser(id, user){
+    // sessionIdToUserMap.set(id, user);
+    return jwt.sign({
+        _id: user._id,
+        email: user.email,
+    }, secret);
 }
 
-function getUser(id){
-    return sessionIdToUserMap.get(id);
+// function getUser(id){
+//     return sessionIdToUserMap.get(id);
+// }
+
+function getUser(token){
+    if (!token) return null;
+    try{
+        return jwt.verify(token, secret);
+    }
+    catch(error){
+        return null;
+    }
 }
 
 module.exports = {
