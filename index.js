4918130929cd23fec4f2fb3cb8a169baed72275c
Vermasaiyam <vermasaiyam9@gmail.com>
2024-09-12 20:11:10 +0530

    project

---
+++ index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cookieParser = require('cookie-parser');
+
+const {connectToMongoDb} = require('./connect');
+const { restrictedToLoggedInUserOnly, checkAuth } = require("./middleware/auth");
+
+const path = require('path');
+
+//ROUTES
+const urlRoute = require('./routes/url');
+const staticRoute = require('./routes/staticRouter');
+const userRoute = require('./routes/user');
+
+const URL = require('./models/url');
+
+const app = express();
+const PORT = 8001;
+
+connectToMongoDb("mongodb://127.0.0.1:27017/short-url")
+.then(()=> console.log("MongoDB connected"));
+
+// for server side rendering - ejs (more egs. pug.js, handlebars.js)
+app.set('view engine', 'ejs');
+// ejs ko btana ki hmari files kha pdi h ejs ki
+app.set('views', path.resolve('./views'));
+
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+
+app.use('/',checkAuth,staticRoute);
+app.use('/url',restrictedToLoggedInUserOnly,  urlRoute);
+app.use('/user', userRoute);
+
+app.get('/url/:shortId', async (req,res)=>{
+    const shortId = req.params.shortId;
+    const entry = await URL.findOneAndUpdate(
+    {
+        shortId,
+    },
+    { 
+        $push : {
+        visitHistory: {
+            timestamp: Date.now(),
+        },
+    }})
+    res.redirect(entry.redirectUrl);
+})
+
+
+app.listen(PORT, ()=> console.log(`Server Started at PORT: ${PORT}`));
---
+++ middleware/auth.js
@@ -0,0 +1,31 @@
+const { getUser } = require("../services/auth");
+
+async function restrictedToLoggedInUserOnly(req, res, next){
+    const userUid = req.cookies?.uid;
+
+    if (!userUid){
+        return res.redirect('/login');
+    }
+    const user = getUser(userUid);
+
+    if (!user){
+        return res.render('/login');
+    }
+
+    req.user = user;
+    next();
+}
+
+async function checkAuth(req, res, next){
+    const userUid = req.cookies?.uid;
+
+    const user = getUser(userUid);
+
+    req.user = user;
+    next();
+}
+
+module.exports = {
+    restrictedToLoggedInUserOnly,
+    checkAuth,
+};
